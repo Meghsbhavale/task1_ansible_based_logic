111b19ef01bea333d54f81a6174b63ed6b06b686
Kunal Jha <kunaljha5@gmail.com>
2018-12-18 00:38:52 +0530

    added the script logic to compare the version on deployed machines

--- report_final.sh
+++ report_final.sh
@@ -25,3 +25,45 @@ then
 else
 	echo "Process of Final Report file failed."
 fi
+
+
+dest_node=$1
+src_node=$2
+
+
+
+root_master_compare()
+{
+DEST=$(cat   $HOME/test/report/final/report_final.csv |grep 'module-name'| tr ',' "\n"| nl | grep "$dest_node"| awk '{print $1}')
+SOURCE=$(cat   $HOME/test/report/final/report_final.csv |grep 'module-name'| tr ',' "\n"|nl | grep "$src_node"| awk '{print $1}')
+
+cat $HOME/test/report/final/report_final.csv | grep -v "module-name"|cut -d, -f1,$SOURCE >  $HOME/test/report/final/report_final1.csv
+cat $HOME/test/report/final/report_final.csv | grep -v "module-name"|cut -d, -f$DEST >  $HOME/test/report/final/report_final2.csv
+paste -d,  $HOME/test/report/final/report_final1.csv  $HOME/test/report/final/report_final2.csv > $HOME/test/report/final/report_finalR.csv
+rm -rf $HOME/test/report/final/report_final1.csv  $HOME/test/report/final/report_final2.csv
+
+cat $HOME/test/report/final/report_finalR.csv | grep -v "module-name" |while read line
+do
+        Name=$(echo $line|cut -d, -f1)
+        Ver1=$(echo $line|cut -d, -f2)
+        Ver2=$(echo $line|cut -d, -f3)
+
+                if [[ $Ver1 == "-" ]]; then
+                        echo "No action to be taken" 2>/dev/null 1>/dev/null
+                elif [[ $Ver1 > $Ver2  ]]; then
+                        echo "$Name,$Ver1"
+                elif [[ "-" == "$Ver2" ]]; then
+                        echo "$Name,$Ver1"
+                elif [[ $Ver1 < $Ver2  ]]; then
+                        echo "No Action to be taken" 2>/dev/null 1>/dev/null
+                elif [[ $Ver1 == $Ver2  ]]; then
+                        echo "No Action to be taken" 2>/dev/null 1>/dev/null
+                else
+                        echo "No Action to be taken" 2>/dev/null 1>/dev/null
+                fi
+
+done
+rm -rf $HOME/test/report/final/report_finalR.csv
+}
+root_master_compare > $HOME/test/report/final/report_filter.csv
+
